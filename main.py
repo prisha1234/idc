ffbce084e0962439c6824b28aba16ad266f1befd
prisha12341 <93686097+prisha1234@users.noreply.github.com>
2021-11-13 00:33:05 +0000

    not much just made the whole project in like 10 sec no big deal

---
+++ main.py
@@ -0,0 +1,11 @@
+print('this is a bot of max and min indicator')
+a = input("enter the first number")
+b = input("-`enter 2nd number")
+if (a>b): 
+  print ("a is max out of both")
+  print ("b is min of both")
+elif (a==b):
+  print ("both r equal")
+elif (b>a):
+    print("b is greater  than a")
+    print("a is the min of both")
